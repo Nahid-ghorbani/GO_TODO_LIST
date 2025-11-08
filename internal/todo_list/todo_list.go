758f53800116ef1f49dc377300194f3305c3c16b
Nahid <ghorbani.nca@gmail.com>
2025-11-08 23:22:21 +0330

    writing task on json file

--- internal/todo_list/todo_list.go
+++ internal/todo_list/todo_list.go
@@ -1,7 +1,9 @@
 package todo_list
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/Nahid-ghorbani/GO_TODO_LIST/internal/todo_list/model"
 )
@@ -12,8 +14,31 @@ func CreateNewTask(args *Args) model.TaskType {
 		Description: args.description,
 		Status:      model.StatusType(args.status),
 	}
-
 	return task
+}
+
+func WriteOnTheJsonFile(task model.TaskType, fileName string) error {
+	var tasks []model.TaskType
+
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return fmt.Errorf("failed to open the file: %v", err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&tasks); err != nil && err.Error() != "EOF" {
+		return fmt.Errorf("failed to decode tasks from file: %v", err)
+	}
+
+	tasks = append(tasks, task)
+	file.Seek(0,0)
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(&tasks); err != nil {
+		return fmt.Errorf("failed to encode tasks to file: %v", err)
+	}
+
+	return nil
 }
 
 func RunTodo() {
@@ -24,5 +49,12 @@ func RunTodo() {
 	}
 
 	newTask := CreateNewTask(taskValues)
+	
+	err = WriteOnTheJsonFile(newTask, "tasks.json")
+	if err != nil {
+		fmt.Println("error writing task on json file:", err)
+		return
+	}
+
 	fmt.Println("new task", newTask)
 }
